911cfed7ea27d7e0c7e776cec0795d0d1bf74ea2
ImaniKadiva7 <105231479+ImaniKadiva7@users.noreply.github.com>
2023-02-17 13:09:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function alertUser()
 {
-  alert("This project was developed as progress in my Front end development journey by Imani Kadiva");
+  alert("This project was developed as progress in my Front end development journey by Imani Noah Kadiva");
 }
